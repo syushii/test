a82465d82d042457c6981b1ade940f9babdc5c2c
syushii <zzwsyushii@gmail.com>
2021-07-03 10:09:32 +0900

    xxx

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+    /* Output Java--Hello World
+
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
